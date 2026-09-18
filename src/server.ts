814d6c8ee97ba8f31164f9848c58bbe5d5b6c4dd
Ziad003 <ziad.hossain15@gmail.com>
2026-09-18 12:20:51 +0600

    created path between expressServer and Cloud postgress db service

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,31 @@
-import express, { type Application, type Request, type Response } from "express";
-const app : Application = express();
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import { Pool } from "pg";
+
+const app: Application = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
+const pool = new Pool({
+  connectionString:
+    "postgresql://neondb_owner:npg_m0oetXDrEJ3C@ep-rapid-mountain-b5mb6uhh-pooler.c-7.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+});
+
 app.get("/", (req: Request, res: Response) => {
-//   res.send("Hello World!");
+  //   res.send("Hello World!");
   res.status(200).json({ message: "This is root" });
 });
 
-app.post("/",(req:Request,res:Response)=>{
-    // console.log(req.body)
-    const {name,email,password}=req.body;
-    res.status(201).json({message: "Created", data: {name,email}});
+app.post("/", (req: Request, res: Response) => {
+  // console.log(req.body)
+  const { name, email, password } = req.body;
+  res.status(201).json({ message: "Created", data: { name, email } });
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
