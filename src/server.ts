cdb662b1335ef220e9334b3c73e4a61e659e5d6f
Ziad003 <ziad.hossain15@gmail.com>
2026-09-18 12:39:21 +0600

    connetct db and create table

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,27 @@ const pool = new Pool({
     "postgresql://neondb_owner:npg_m0oetXDrEJ3C@ep-rapid-mountain-b5mb6uhh-pooler.c-7.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
 });
 
+const initDb= async ()=>{
+    try {
+        await pool.query(`
+                CREATE TABLE IF NOT EXISTS users (
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(20) NOT NULL,
+                    email VARCHAR(50) NOT NULL,
+                    password VARCHAR(20) NOT NULL,
+                    is_active BOOLEAN DEFAULT true,
+
+                    crated_at TIMESTAMP DEFAULT NOW(),
+                    updated_at TIMESTAMP DEFAULT NOW()
+         )
+            `)
+            console.log("Database connected successfully")
+    } catch (error) {
+        console.log(error)
+    }
+}
+initDb();
+
 app.get("/", (req: Request, res: Response) => {
   //   res.send("Hello World!");
   res.status(200).json({ message: "This is root" });
