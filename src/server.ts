25b770b6ef4ec3566f195a6f53a56cec5a4eeef1
Ziad003 <ziad.hossain15@gmail.com>
2026-09-18 12:06:47 +0600

    Post request

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,18 @@ import express, { type Application, type Request, type Response } from "express"
 const app : Application = express();
 const port = 3000;
 
-// app.use(express.json());
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
 //   res.send("Hello World!");
   res.status(200).json({ message: "This is root" });
 });
 
+app.post("/",(req:Request,res:Response)=>{
+    console.log(req.body)
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
