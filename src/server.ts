5ec4a20fbc09eb4b83f8e02cbbb41676f1963c9c
Ziad003 <ziad.hossain15@gmail.com>
2026-09-18 11:56:07 +0600

    use get method

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import express, { type Request, type Response } from "express";
-const app = express();
+import express, { type Application, type Request, type Response } from "express";
+const app : Application = express();
 const port = 3000;
 
-app.use(express.json());
+// app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+//   res.send("Hello World!");
   res.status(200).json({ message: "This is root" });
 });
 
